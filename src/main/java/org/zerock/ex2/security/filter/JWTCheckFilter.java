0a5a4e16c191c3c36e08ca0b22ea307400eb252d
niftus13 <niftus13@gmail.com>
2023-08-02 12:19:27 +0900

    권한 체크 하기 위해 claims 검사

--- src/main/java/org/zerock/ex2/security/filter/JWTCheckFilter.java
+++ src/main/java/org/zerock/ex2/security/filter/JWTCheckFilter.java
@@ -6,11 +6,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerock.ex2.dto.MemberDTO;
 import org.zerock.ex2.util.JWTUtil;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Map;
 
 @Log4j2
@@ -46,6 +50,23 @@ public class JWTCheckFilter extends OncePerRequestFilter {
             Map<String, Object> claims = JWTUtil.validateToken(accessToken);
 
             log.info("JWT claims: " + claims);
+
+            String email = (String) claims.get("email");
+            String pw = (String) claims.get("pw");
+            String nickname = (String) claims.get("nickname");
+            Boolean social = (Boolean) claims.get("social");
+            List<String> roleNames = (List<String>) claims.get("roleNames");
+
+            MemberDTO memberDTO = new MemberDTO(email, pw, nickname, social.booleanValue(), roleNames);
+
+            log.info("-----------------------------------");
+            log.info(memberDTO);
+            log.info(memberDTO.getAuthorities());
+
+            UsernamePasswordAuthenticationToken authenticationToken
+                    = new UsernamePasswordAuthenticationToken(memberDTO, pw, memberDTO.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
             filterChain.doFilter(request, response);
 
