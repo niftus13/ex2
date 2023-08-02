b7ae9fed3deca77cd389abe10867359e1795e68c
niftus13 <niftus13@gmail.com>
2023-08-02 12:03:10 +0900

    jwtfilter 후 체크

--- src/main/java/org/zerock/ex2/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/ex2/config/CustomSecurityConfig.java
@@ -11,9 +11,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.zerock.ex2.security.filter.JWTCheckFilter;
 import org.zerock.ex2.security.handler.ApiLoginSuccessHandler;
 
 import java.util.Arrays;
@@ -45,6 +47,8 @@ public class CustomSecurityConfig {
 
         //세션/쿠키 사용안함
         http.sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(new JWTCheckFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/main/java/org/zerock/ex2/security/filter/JWTCheckFilter.java
+++ src/main/java/org/zerock/ex2/security/filter/JWTCheckFilter.java
@@ -4,19 +4,33 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Log4j2
 public class JWTCheckFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return super.shouldNotFilter(request);
+
+
+        String path = request.getRequestURI();  // uri 경로
+        if(path.equals("/api/member/login")){
+            return true;
+        }
+
+
+        return false;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        log.info("----------------------doFilterInternal -----------------------");
+
+        // 필터 통과후 다음 필터나 컨트롤러들을 호출하는 것
+        filterChain.doFilter(request,response);
     }
 }
