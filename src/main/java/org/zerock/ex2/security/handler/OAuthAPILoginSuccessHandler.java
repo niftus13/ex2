8a505ae59c1765bf69633ef468db7a6ca1e4a826
niftus13 <niftus13@gmail.com>
2023-08-03 11:25:23 +0900

    카카오 로그인

--- src/main/java/org/zerock/ex2/security/handler/OAuthAPILoginSuccessHandler.java
+++ src/main/java/org/zerock/ex2/security/handler/OAuthAPILoginSuccessHandler.java
@@ -1,14 +1,22 @@
 package org.zerock.ex2.security.handler;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.google.gson.Gson;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.log4j.Log4j2;
+import org.zerock.ex2.dto.MemberDTO;
+import org.zerock.ex2.util.JWTUtil;
+
 
 @Log4j2
 public class OAuthAPILoginSuccessHandler implements AuthenticationSuccessHandler{
@@ -19,11 +27,37 @@ public class OAuthAPILoginSuccessHandler implements AuthenticationSuccessHandler
 
 
         log.info("OAuth onAuthenticationSuccess.................");
-        log.info("OAuth onAuthenticationSuccess.................");
-        log.info("OAuth onAuthenticationSuccess.................");
-        log.info("OAuth onAuthenticationSuccess.................");
-        log.info("OAuth onAuthenticationSuccess.................");
+
+
+        String email = authentication.getName();
+
+        log.info("SEND TO APPLICATION: " + email);
+
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+
+        Map<String, Object> claims = memberDTO.getClaims();
+
+        String accessToken = JWTUtil.generateToken(claims, 10);
+
+        String refreshToken = JWTUtil.generateToken(claims, 60 * 24);
+
+        Gson gson = new Gson();
+
+        Map<String, Object> dataMap = new HashMap<>();
+
+        dataMap.putAll(claims);
+
+        dataMap.put("accessToken", accessToken);
+        dataMap.put("refreshToken", refreshToken);
+
+        String jsonStr = gson.toJson(dataMap);
+
+        String encodeStr = URLEncoder.encode(jsonStr, "UTF-8");
+
+        response.sendRedirect("http://localhost:3000/member/oauthResult?data="+encodeStr);
 
     }
 
 }
+
+
